37d9c2d5108baea094fed6c8f804ab2ff71cf02d
lmutton <lornamutton@gmail.com>
2017-10-05 13:23:24 +0200

    updated canvas with changeable color and size

--- 08Canvas/js/index.js
+++ 08Canvas/js/index.js
@@ -1,24 +1,26 @@
 const canvas = document.querySelector("#draw");
+const penColor = document.querySelector("#pen-color");
+const penSize = document.querySelector("#pen-size");
+const clear = document.querySelector(".clear");
+const multi = document.querySelector(".multi");
 const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+//const inputs = document.querySelectorAll('.controlls input');
+canvas.width = 600;
+canvas.height = 600;
 
-ctx.strokeStyle = "color";
+ctx.strokeStyle = penColor.value;
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
-ctx.lineWidth = 100;
+ctx.lineWidth = penSize.value;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
-let size = true;
 
 function draw(e) {
 	//stop function from running if not mousedown
 	if (!isDrawing) return;
-	//set stroke colour
-	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 	//beginpath
 	ctx.beginPath();
 	//start from
@@ -29,22 +31,14 @@ function draw(e) {
 	ctx.stroke();
 	//last x to equal offsetX and last y to equal offsetY
 	[lastX, lastY] = [e.offsetX, e.offsetY];
+}
 
-	//change colour
-	hue++;
-	if (hue === 360) {
-		hue = 0;
-	}
+function clearCanvas() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-	//change pen size
-	if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-		size = !size;
-	}
-	if (size) {
-		ctx.lineWidth++;
-	} else {
-		ctx.lineWidth--;
-	}
+function handleHover() {
+	document.documentElement.style.setProperty(`--${this.name}`, this.value);
 }
 
 canvas.addEventListener("mousedown", e => {
@@ -52,9 +46,13 @@ canvas.addEventListener("mousedown", e => {
 	isDrawing = true;
 	//when mousedown lastX and lastY should = offset and not 0
 	[lastX, lastY] = [e.offsetX, e.offsetY];
+	ctx.strokeStyle = penColor.value;
+	ctx.lineWidth = penSize.value;
 });
 //when mouse moves draw
 canvas.addEventListener("mousemove", draw);
 //but if mouse is up or out then set isDrawing to false
 canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mouseout", () => (isDrawing = false));
+clear.addEventListener("click", clearCanvas);
+penColor.addEventListener("change", handleHover);
